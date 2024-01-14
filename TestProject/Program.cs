b7876e49507ca13ab40ca26d490c861198d18570
IKennCode <dimalibotkennethofficial@gmail.com>
2024-01-14 18:57:37 +0800

    ada

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,13 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
+﻿
 
-bool flag = false;
-if(flag) 
-{
-    int value = 10;
-   
-    Console.WriteLine($"Inside the code block: {value}");
-}else{
-    int valuess = 12;
-    Console.WriteLine($"Inside the code block: {valuess}");
+
+//Control Variable Scope and Logic Using Code Blocks in C#
+
+// bool flag = false;
+// int value = 10;
+// if(flag) 
+// {
+//     Console.WriteLine($"Inside the code block: {value}");
+// }
+
+// Console.WriteLine($"Outside the code block: {value}");
+
+// ***************************************************************
+//Initialize the variable with a value
+// bool flag = true;
+// int value = 0;
+
+// if(flag) {
+//     value = 10;
+//     Console.WriteLine("Inside of the code block: "  + value);
+// }
+// Console.WriteLine("Outside of the code block:" + value);
+
+// *****************************************************************
+// Remove Code blocks from if statements
+bool flag = true;
+if(flag) {
+    Console.WriteLine(flag);
 }
--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e28f727ac32869d73ff96ddcef4fb1728e0582b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0805fb559aa01fe8b63f96b905567af8d4d2e227")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
